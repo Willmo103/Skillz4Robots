a411009656f65ab6e956e96efdab41b3169a3f59
Will Morris <willmorris103@gmail.com>
2024-06-06 04:32:16 -0500

    Refactoring adding DirectoryIngestor

--- skillz/file_skills/directory_skills.py
+++ skillz/file_skills/directory_skills.py
@@ -3,7 +3,15 @@ import os
 import logging
 import fnmatch
 
-logging.basicConfig(level=logging.DEBUG)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filemode='a',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%d-%b-%y %H:%M:%S',
+    stream=None,
+)
+logging.getLogger().setLevel(logging.DEBUG)
 _log = logging.getLogger(__name__)
 
 
@@ -43,14 +51,13 @@ class FileIgnore:
         return False
 
     def add_ignore_pattern(self, pattern):
-        if pattern not in self.ignore_patterns or pattern:
+        if pattern not in self.ignore_patterns and pattern not in self.except_patterns:
             self.ignore_patterns.append(pattern)
             self.save_ignore()
 
     def add_except_pattern(self, pattern):
-        if pattern not in self.except_patterns:
-            self.except_patterns.append(pattern)
-            self.save_ignore()
+        self.except_patterns.append(pattern)
+        self.save_ignore()
 
     def save_ignore(self):
         with open('ignore.json', 'w') as f:
@@ -66,18 +73,30 @@ class FileIgnore:
         return self.ignore_patterns, self.except_patterns
 
 
+class File:
+    def __init__(self, path, content, visited=False, chunk_pattern="\n\n"):
+        self.path = path
+        self.content = content
+        self.visited = visited
+        self.chunk_pattern = chunk_pattern
+        self.chunks = self.split_into_chunks()
+
+    def split_into_chunks(self):
+        return self.content.split(self.chunk_pattern)
+
+
 class DirectoryIngestor:
-    def __init__(self, path):
+    def __init__(self, path, file_ignore):
         self.path = path
         self.structure = {}
-        self.matcher = FileIgnore()
+        self.matcher = file_ignore
+        self.files = []
+        self.folders = []
         try:
             self.structure = self.get_structure(self.path)
         except Exception as e:
             _log.error(f'Could not initialize Codebase: {e}')
             raise Exception('Could not initialize Codebase')
-        self.files = []
-        self.folders = []
 
     def get_structure(self, path: str):
         structure = {}
@@ -87,26 +106,34 @@ class DirectoryIngestor:
                 if self.matcher.should_ignore(item):
                     continue
                 if os.path.isdir(item_path):
-                    subdir = self.get_structure(item_path)
-                    if subdir:
-                        structure[item] = subdir
+                    subdir_ingestor = DirectoryIngestor(item_path, self.matcher)
+                    if subdir_ingestor.structure:
+                        structure[item] = subdir_ingestor.structure
                         self.folders.append(item_path)
                 else:
                     if self.matcher.should_except(item):
                         try:
                             with open(item_path, 'r') as f:
                                 content = f.read()
-                                _file = {
-                                    "path": item_path,
-                                    "content": content,
-                                    "visited": False
+                                file_obj = File(path=item_path, content=content)
+                                self.files.append(file_obj)
+                                structure[item] = {
+                                    "path": file_obj.path,
+                                    "content": file_obj.content,
+                                    "visited": file_obj.visited,
+                                    "chunks": file_obj.chunks
                                 }
-                                self.files.append(_file)
-                                structure[item] = _file
+                        except PermissionError as e:
+                            _log.error(f'Could not read file {item_path}: {e}')
+                            structure[item] = {}
+                        except BlockingIOError as e:
+                            _log.error(f'Could not read file {item_path}: {e}')
+                            structure[item] = {}
                         except Exception as e:
                             _log.error(f'Could not read file {item_path}: {e}')
                             extension = os.path.splitext(item)[1]
-                            self.matcher.add_ignore_pattern(f'*{extension}')
+                            if extension:
+                                self.matcher.add_ignore_pattern(f'*{extension}')
                             structure[item] = {}
         except Exception as e:
             _log.error(f'Error traversing path {path}: {e}')
@@ -114,5 +141,6 @@ class DirectoryIngestor:
 
 
 # Example usage:
-codebase = DirectoryIngestor('../')
+fi = FileIgnore()
+codebase = DirectoryIngestor('../../../', fi)
 print(json.dumps(codebase.structure, indent=4))
